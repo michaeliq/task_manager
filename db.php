4fe85cfab1331d9620b52a566f87f4f033ca0312
Josue <michaelesacol@gmail.com>
2021-08-10 13:56:40 -0500

    actualización archivo db

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
     mysqli_report(MYSQLI_REPORT_STRICT);
 
     ob_start();
-    ?>
+?>
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <strong>Connection Issues!</strong>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -11,20 +11,22 @@
         </button>
     </div>
 
-    <?php
+<?php
 
     $buffer = ob_get_contents();
 
     ob_end_clean();
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
     try {
-        $conn = mysqli_connect(
-            $host = "localhost",
-            $username = "root",
-            $password = "",
-            $db = "crud"
-        );
+        $conn = mysqli_connect($server,$username,$password,$db);
     } catch (Exception $e) {
-        echo $buffer;
+        echo "error en conexión";
     }
     
 
