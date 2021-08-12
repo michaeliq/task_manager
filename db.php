36a6070016bddc759bb2bc0b7fe9b4e24c1af170
Josue <michaelesacol@gmail.com>
2021-08-12 10:16:56 -0500

    update db

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php 
 
     $conn = mysqli_connect(
-        $hostname="localhost",
-        $username="root",
-        $password="",
-        $database="crud"
+        $hostname="us-cdbr-east-04.cleardb.com",
+        $username="b8f09b67c29dc8",
+        $password="394e1ac8",
+        $database="heroku_a75d4c0bb9904be";
     )
 
 ?>
