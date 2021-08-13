459c69b42c513a52c17cc092f841965e55b72082
Josue <michaelesacol@gmail.com>
2021-08-13 08:17:30 -0500

    corección de db

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
         $hostname="us-cdbr-east-04.cleardb.com",
         $username="b8f09b67c29dc8",
         $password="394e1ac8",
-        $database="heroku_a75d4c0bb9904be";
-    )
+        $database="heroku_a75d4c0bb9904be"
+    );
 
 ?>
